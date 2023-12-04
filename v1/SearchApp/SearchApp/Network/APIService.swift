9ec2aea1a5a7e2d894d3ea48676789f06a4c62f2
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-12-04 20:09:39 +0900

    feat #1 add fetchSuggestion
    검색어 추천 api 패처 추가구현

--- v1/SearchApp/SearchApp/Network/APIService.swift
+++ v1/SearchApp/SearchApp/Network/APIService.swift
@@ -15,6 +15,37 @@ class APIService {
 
     }
 
+    func fetchSuggestion(url: URL?, completion: @escaping (Result<Suggestion, APIError>) -> Void) {
+        guard let url = url else {
+            completion(Result.failure(APIError.invalidURL))
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(Result.failure(APIError.transportError(error)))
+            }
+
+            if let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) == false {
+                completion(Result.failure(APIError.badResponse(stateCode: response.statusCode)))
+            }
+
+            if let data = data {
+                let decoder = SuggestionXMLParser()
+
+                do {
+                    let result = try decoder.xmlDecode(data: data)
+                    completion(Result.success(result))
+                } catch {
+                    completion(Result.failure(APIError.parsingError))
+                }
+
+            }
+        }
+
+        task.resume()
+    }
+
     func fetchSearchWebResult(request: URLRequest?, completion: @escaping (Result<WebSearch, APIError>) -> Void) {
         guard let request = request else {
             let error = APIError.invalidRequest
