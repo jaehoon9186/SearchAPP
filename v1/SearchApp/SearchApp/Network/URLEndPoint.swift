5aeaf5518a34d3f04518ac9c4f706d498c4dfa52
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-12-28 21:27:48 +0900

    update #1 URLEndPoint
    다음페이지를 위한 매개변수 추가

--- v1/SearchApp/SearchApp/Network/URLEndPoint.swift
+++ v1/SearchApp/SearchApp/Network/URLEndPoint.swift
@@ -8,22 +8,22 @@
 import Foundation
 
 enum EndPoint {
-    case web(query: String)
-    case video(query: String)
-    case image(query: String)
+    case web(query: String, page: Int)
+    case video(query: String, page: Int)
+    case image(query: String, page: Int)
 
     var url: URL? {
         let baseURL = "https://dapi.kakao.com/v2/search"
 
         switch self {
-        case .web(let query):
-            let str = "\(baseURL)/web?query=\(query)"
+        case .web(let query, let page):
+            let str = "\(baseURL)/web?query=\(query)&page=\(page)"
             return URL(string: str.encodeURL()!)
-        case .video(let query):
-            let str = "\(baseURL)/vclip?query=\(query)"
+        case .video(let query, let page):
+            let str = "\(baseURL)/vclip?query=\(query)&page=\(page)"
             return URL(string: str.encodeURL()!)
-        case .image(let query):
-            let str = "\(baseURL)/image?query=\(query)"
+        case .image(let query, let page):
+            let str = "\(baseURL)/image?query=\(query)&page=\(page)"
             return URL(string: str.encodeURL()!)
         }
     }
