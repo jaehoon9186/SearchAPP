d11a342ebcb68ce78abd3b82274c4a3c66136e92
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-12-28 21:26:18 +0900

    refactor #8 #1 api call func
    재사용성 증가, 모듈화

--- v1/SearchApp/SearchApp/Network/APIService.swift
+++ v1/SearchApp/SearchApp/Network/APIService.swift
@@ -12,6 +12,42 @@ class APIService {
     static let shared = APIService()
 
     init() {
+    func fetchSearchResult<T: Decodable>(_ type: T.Type, request: URLRequest?, completion: @escaping (Result<T, APIError>) -> Void) {
+
+        guard let request = request else {
+            let error = APIError.invalidRequest
+            completion(Result.failure(error))
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+
+            if let error = error {
+                completion(Result.failure(APIError.transportError(error)))
+            }
+
+            if let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) == false {
+                completion(Result.failure(APIError.badResponse(stateCode: response.statusCode)))
+            }
+
+            if let data = data {
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.sssZ"
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .formatted(formatter)
+
+                do {
+                    let result = try decoder.decode(type, from: data)
+                    completion(Result.success(result))
+                } catch {
+                    completion(Result.failure(APIError.parsingError))
+                }
+
+            }
+
+        }
+
+        task.resume()
 
     }
 
