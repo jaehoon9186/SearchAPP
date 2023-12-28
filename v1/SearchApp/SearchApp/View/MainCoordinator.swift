728b18ce16a88c5cbd9c5022f1db906f894e39c9
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-12-28 21:37:49 +0900

    update #5 MainCoordinator

--- v1/SearchApp/SearchApp/View/MainCoordinator.swift
+++ v1/SearchApp/SearchApp/View/MainCoordinator.swift
@@ -15,8 +15,15 @@ class MainCoordinator: Coordinator {
         self.navigationController = navigationController
     }
 
+    // toError
+
+    // toDetailView
+    
+
     func start() {
-        let vc = MainViewController()
+        
+        let vc = SearchViewController()
+        vc.coordinator = self
         self.navigationController?.viewControllers = [vc]
     }
 }
