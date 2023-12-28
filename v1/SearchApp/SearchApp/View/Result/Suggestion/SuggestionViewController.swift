fe55ac8b401dfb175a8e7cf3767680b2b26a0663
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-12-28 21:33:59 +0900

    add #7 SuggestionViewController

---
+++ v1/SearchApp/SearchApp/View/Result/Suggestion/SuggestionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SuggestionViewController.swift
+//  SearchApp
+//
+//  Created by LeeJaehoon on 2023/12/15.
+//
+
+import UIKit
+
+class SuggestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .systemPink
+    }
+
+
+}
