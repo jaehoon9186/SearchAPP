84b91cb1bb6ba734371d60660e33239f5ebe39d6
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2024-01-01 12:00:58 +0900

    update #7 add method for Suggestion

--- v1/SearchApp/SearchApp/View/Search/SearchViewModel.swift
+++ v1/SearchApp/SearchApp/View/Search/SearchViewModel.swift
@@ -13,6 +13,9 @@ class SearchViewModel {
         case webButtonTap(query: String, page: Int = 1)
         case imageButtonTap(query: String, page: Int = 1)
         case videoButtonTap(query: String, page: Int = 1)
+        case searchingGetRecord(query: String) // 검색 기록
+        case searchingGetSuggestion(query: String) // 추천 검색
+        case recordRemoveButtonTap(object: SearchRecord) // 검색 기록 cell 삭제
     }
 
     enum Output {
@@ -20,6 +23,8 @@ class SearchViewModel {
         case fetchWebSucceed(result: WebSearch, nowPage: Int)
         case fetchImageSucceed(result: ImageSearch, nowPage: Int)
         case fetchVideoSucceed(result: VideoSearch, nowPage: Int)
+        case fetchSuggestionSucceed(result: Suggestion)
+        case fetchRelatedRecordSucceed(result: [SearchRecord])
     }
 
     private let apiService: APIService
@@ -36,10 +41,22 @@ class SearchViewModel {
             switch event {
             case .webButtonTap(let query, let page):
                 self?.handleGetSearchResult(type: WebSearch.self, url: EndPoint.web(query: query, page: page).url, nowPage: page)
+                fallthrough
             case .imageButtonTap(let query, let page):
                 self?.handleGetSearchResult(type: ImageSearch.self, url: EndPoint.image(query: query, page: page).url, nowPage: page)
+                fallthrough
             case .videoButtonTap(let query, let page):
                 self?.handleGetSearchResult(type: VideoSearch.self, url: EndPoint.video(query: query, page: page).url, nowPage: page)
+                fallthrough
+            case .webButtonTap(let query, _), .imageButtonTap(let query, _), .videoButtonTap(let query, _):
+                self?.saveRecordWord(query: query)
+            case .searchingGetRecord(let query):
+                self?.handleGetRecordWords(query: query)
+            case .searchingGetSuggestion(let query):
+                self?.handleGetSuggestion(query: query)
+            case .recordRemoveButtonTap(let object):
+                // 리무빙.
+                print("삭제 \(object)")
             }
         }.store(in: &cancellables)
 
@@ -64,7 +81,6 @@ class SearchViewModel {
         } receiveValue: { [weak self] result in
             switch type {
             case is WebSearch.Type:
-                print("output send 했나")
                 self?.output.send(Output.fetchWebSucceed(result: result as! WebSearch, nowPage: nowPage))
             case is ImageSearch.Type:
                 self?.output.send(Output.fetchImageSucceed(result: result as! ImageSearch, nowPage: nowPage))
@@ -77,4 +93,51 @@ class SearchViewModel {
 
     }
 
+    func handleGetRecordWords(query: String) {
+        let str = query.trimmingCharacters(in: .whitespacesAndNewlines)
+        let count = str.isEmpty ? 10 : 3
+
+        CoreDataManager.shard.readRecord().publisher.sink { [weak self] records in
+
+            var result = records.filter { $0.word!.hasPrefix(str) }
+            result = zip(result, (0..<count)).map { $0.0 }
+
+            self?.output.send(.fetchRelatedRecordSucceed(result: result))
+        }.store(in: &cancellables)
+    }
+    
+    func handleGetSuggestion(query: String) {
+        let str = query.trimmingCharacters(in: .whitespacesAndNewlines)
+        if str.isEmpty {
+            self.output.send(.fetchSuggestionSucceed(result: Suggestion(suggestedWords: [])))
+            return
+        }
+
+        let urlStr = "https://suggestqueries.google.com/complete/search?output=toolbar&hl=kor&q=\(str)"
+        let url = URL(string: urlStr.encodeURL()!)
+
+        apiService.getFetchSuggestion(url: url).sink { completion in
+            switch completion {
+            case .failure(let error):
+                print("에러떳너")
+            case .finished:
+                print("끝")
+            }
+        } receiveValue: { [weak self] suggestion in
+            self?.output.send(.fetchSuggestionSucceed(result: suggestion))
+        }.store(in: &cancellables)
+    }
+
+    func saveRecordWord(query: String) {
+        let str = query.trimmingCharacters(in: .whitespacesAndNewlines)
+        if str.isEmpty {
+            return
+        }
+
+        CoreDataManager.shard.saveRecord(word: str)
+    }
+
+    func removeAllRecordWords() {
+        CoreDataManager.shard.deleteAll()
+    }
 }
